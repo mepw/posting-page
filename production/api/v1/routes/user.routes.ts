6ce8a1f0f9bcc8b6353629bd902419e68a064985
unknownmeowmeow <164880015+unknownmeowmeow@users.noreply.github.com>
2025-12-03 14:56:17 +0800

    Fix: Corrected typo on refresh route

--- production/api/v1/routes/user.routes.ts
+++ production/api/v1/routes/user.routes.ts
@@ -5,5 +5,5 @@ import UserController from "../controllers/users.controller"
 const AuthRoute: Router = Router();
 AuthRoute.get("/user", exec(UserController.getUserId));
 AuthRoute.post("/logout", exec(UserController.logOutUser));
-AuthRouteq.post("/refresh", exec(UserController.refreshToken));
+
 export default AuthRoute;
