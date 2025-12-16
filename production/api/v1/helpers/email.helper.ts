87c2e914b22c59551a49522e7a7d5f3c0d29f971
unknownmeowmeow <164880015+unknownmeowmeow@users.noreply.github.com>
2025-12-16 08:35:20 +0800

    update the spacing in the email helper

--- production/api/v1/helpers/email.helper.ts
+++ production/api/v1/helpers/email.helper.ts
@@ -2,39 +2,27 @@ import nodemailer from "nodemailer";
 import { SMTP_USER, SMTP_PASS, SMTP_FROM } from "../../../configs/constants/env.constant"; // adjust path
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
-  auth: {
-    user: SMTP_USER,
-    pass: SMTP_PASS,
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASS,
+    },
 });
 
-export const sendEmail = async ({
-  to,
-  subject,
-  text,
-  html,
-}: {
-  to: string;
-  subject: string;
-  text: string;
-  html?: string;
+export const sendEmail = async ({ to, subject, text, html, }:{
+    to: string;
+    subject: string;
+    text: string;
+    html?: string;
 }) => {
-  try {
-    const info = await transporter.sendMail({
-      from: SMTP_FROM,
-      to,
-      subject,
-      text,
-      html,
-    });
 
-    console.log("Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("Error sending email:", error);
-    throw error;
-  }
+    try{
+        const info = await transporter.sendMail({ from: SMTP_FROM, to, subject, text, html, });
+        return info;
+    }
+    catch(error){
+        throw error;
+    }
 };
