7f5ed343bf140b959bcb56b272532c54a0c37f38
unknownmeowmeow <164880015+unknownmeowmeow@users.noreply.github.com>
2025-12-19 11:07:02 +0800

    remove the unused in user routes

--- production/api/v1/routes/user.routes.ts
+++ production/api/v1/routes/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { exec } from "../helpers/global.helper";
 import UserController from "../controllers/users.controller"
 import { validateSignUpUser } from "../validation/user.validation";
-import { auth } from "googleapis/build/src/apis/abusiveexperiencereport";
+
 const AuthRoute: Router = Router();
 AuthRoute.get("/user", exec(UserController.getUserId));
 AuthRoute.post("/logout", exec(UserController.logOutUser));
